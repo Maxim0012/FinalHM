e7f053448847e650149d2d3fe40fbab0f8930b4b
Maxim <maksim.shishkin@mail.ru>
2023-06-09 23:55:32 +0500

    new program2

--- Семинар 1/ex0/Program.cs
+++ Семинар 1/ex0/Program.cs
@@ -8,6 +8,23 @@ int result = userNumber*userNumber;
 Console.WriteLine($" {userNumber*2} -> {result*2} ");
 */
 
-Console.Clear();
-Console.Write("Введите целое число: ");
+/*Console.Clear();
+Console.Write("Введите целое число 1: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+Console.Write("Введите целое число 2: ");
+string userInput1 = Console.ReadLine() ?? "";
+int userNumber1 = int.Parse(userInput1);
+if ( userNumber == userNumber1*userNumber1);{
+    Console.WriteLine ("yes");}
 
+if ( userNumber != userNumber1*userNumber1);{
+    Console.WriteLine ("no");}
+    */
+
+/*Console.Clear();
+Console.Write("Введите целое число : ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+if()
+*/
