4a680f13425fc87389480eb9d5e627eb0053b818
Maxim <maksim.shishkin@mail.ru>
2023-06-13 23:54:50 +0500

    ex12

--- Семинар 1/ex0/Program.cs
+++ Семинар 1/ex0/Program.cs
@@ -104,6 +104,7 @@ else
 */
 
 //Домашка №8
+/*
 Console.Clear();
 int i = 1;
 
@@ -118,3 +119,36 @@ while (i <= userNumber)
     }
     i++;
 }
+*/
+
+//Задача 11
+/*
+Console.Clear();
+
+int num = new Random().Next(100, 1000);
+int i = num;
+
+int a1 = i / 100;
+
+int a2 = (i % 100) % 10;
+
+Console.WriteLine($"{i} -> {a1}{a2}");
+*/
+
+Console.Clear();
+int a1;
+int a2;
+
+Console.Write("Введите первое число: ");
+a1 = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите второе число: ");
+a2 = int.Parse(Console.ReadLine() ?? "");
+
+if (a1 % a2 == 0)
+{
+    Console.WriteLine($"Число {a1} кратно {a2}");
+
+}
+else
+    Console.WriteLine($"Число {a1} не кратно {a2} остаток {a1 % a2}");
